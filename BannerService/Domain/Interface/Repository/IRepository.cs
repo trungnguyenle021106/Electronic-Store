2b64e3682d37f6456ad1d0065edb83cb8211e2c1
teamweb7 <91516024+trungnguyenle021106@users.noreply.github.com>
2025-02-21 16:54:46 +0700

    BannerService

---
+++ BannerService/Domain/Interface/Repository/IRepository.cs
@@ -0,0 +1,11 @@
+﻿namespace BannerService.Domain.Interface.IRepositories
+{
+    public interface IRepository<TEntity> where TEntity : class
+    {
+        Task<IEnumerable<TEntity>> GetAll();
+        Task<TEntity> GetById(int id);
+        Task<TEntity> Add(TEntity entity);
+        TEntity Update(TEntity entity);
+        void Delete(TEntity entity);
+    }
+}
