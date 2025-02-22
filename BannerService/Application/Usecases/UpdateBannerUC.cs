c85c681d464afce0179c348197a12aa33615aab1
teamweb7 <91516024+trungnguyenle021106@users.noreply.github.com>
2025-02-22 09:20:46 +0700

    Banner Service

---
+++ BannerService/Application/Usecases/UpdateBannerUC.cs
@@ -0,0 +1,34 @@
+﻿
+using BannerService.Application.UnitOfWork;
+using BannerService.Domain.Entities;
+using BannerService.Domain.Interface.UnitOfWork;
+using BannerService.Infrastructure.DBContext;
+
+namespace BannerService.Application.Usecases
+{
+    public class UpdateBannerUC
+    {
+        private readonly IUnitOfWork unitOfWork;
+        public UpdateBannerUC(BannerContext bannerContext)
+        {
+            this.unitOfWork = new BannerUnitOfWork(bannerContext);
+        }
+
+        public async Task<Banner?> UpdateBanner(int bannerID, Banner newBanner)
+        {
+            try
+            {
+                Banner banner = await this.unitOfWork.BannerRepository().GetById(bannerID);
+                banner.Position = newBanner.Position;
+                banner.Image = newBanner.Image;
+                await this.unitOfWork.Commit();
+                return banner;
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Lỗi cập nhật banner : " + ex.Message);
+                return null;
+            }
+        }
+    }
+}
---
+++ BannerService/Infrastructure/DBContext/BannerContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+using BannerService.Domain.Entities;
+
+namespace BannerService.Infrastructure.DBContext
+{
+    public class BannerContext : DbContext
+    {
+        public BannerContext(DbContextOptions<BannerContext> options) : base(options)
+        {
+        }
+
+        public DbSet<Banner> Banners { get; set; }
+
+    }
+}
