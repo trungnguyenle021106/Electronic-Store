55768be022261a0e76ad200101fcd5b6dfc9418b
teamweb7 <91516024+trungnguyenle021106@users.noreply.github.com>
2025-05-09 15:52:19 +0700

    frontend

---
+++ Frontend/src/app/home/home.component.ts
@@ -0,0 +1,9 @@
+import {Component} from '@angular/core';
+import {CommonModule} from '@angular/common';
+@Component({
+  selector: 'app-home',
+  templateUrl: './home.component.html',
+  styleUrls: ['./home.component.css'],
+  standalone: false,
+})
+export class HomeComponent {}
