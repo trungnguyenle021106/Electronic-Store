4a3fce9384428f7ba1e5f8869e66907adb2b3797
teamweb7 <91516024+trungnguyenle021106@users.noreply.github.com>
2025-05-22 15:25:03 +0700

    test

--- ProductService/Domain/Entities/Product.cs
+++ ProductService/Domain/Entities/Product.cs
@@ -5,6 +5,7 @@ namespace ProductService.Domain.Entities
     public class Product
     {
         public int ID { get; set; }
+        public int TypeID { get; set; }
         public string Name { get; set; } = "";
         public int Quantity { get; set; }
         public string Image { get; set; } = "";
---
+++ ProductService/Domain/Entities/ProductType.cs
@@ -0,0 +1,8 @@
+﻿namespace ProductService.Domain.Entities
+{
+    public class ProductType
+    {
+        public int ID { get; set; }
+        public required string Name { get; set; }
+    }
+}
